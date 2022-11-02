a3e2b2bab655209f46d71f9fdb33fe5f12259a6f
sasa <sasa.popravak@gmail.com>
2022-11-02 20:45:34 +0100

    Fixing custom data issues

--- locals.tf
+++ locals.tf
@@ -14,5 +14,6 @@ sudo chmod -R 777 /var/www/html/app1/
 sudo echo "Welcome to App1 - VM: $(hostname)" > /var/www/html/app1/hostname.html
 sudo echo "Welcome to App1 - App Status Page" > /var/www/html/app1/status.html
 sudo echo '<!DOCTYPE html> <html lang="en" xmlns="http://www.w3.org/1999/xhtml"> <head> <meta charset="utf-8" /> <title>App1</title> </head> <body><h1>Welcome to App1</h1>h1></body> </html>' | sudo tee /var/www/html/app1/index.html
+echo ""
 CUSTOM
 }
