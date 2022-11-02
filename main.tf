8f5fd76f66553e24ac0f3a65860dc170089bd476
sasa <sasa.popravak@gmail.com>
2022-11-02 20:16:34 +0100

    Fixing custom data issues

--- main.tf
+++ main.tf
@@ -71,5 +71,6 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   }
 
   // Apply custom configuration to each VM
-  custom_data = base64encode(local.vm_custom_data)
+  //custom_data = base64encode(local.vm_custom_data)
+  custom_data = filebase64("custom_data.sh")
 }
