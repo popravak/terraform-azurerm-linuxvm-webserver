7b7c479561f22a7048d72eff13b6e0b0b1cbdcdd
sasa <sasa.popravak@gmail.com>
2022-11-04 20:33:22 +0100

    Interface configuration  included in outputs

--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,11 @@ output "vm_iface_id" {
   value = azurerm_linux_virtual_machine.linuxvm.*.network_interface_ids[0]
 }
 
+output "vm_iface_config" {
+  description = "VM interface configuration"
+  value = azurerm_network_interface.iface.*.ip_configuration[0]
+}
+
 /*
 output "ssh_public_key" {
   value = azurerm_linux_virtual_machine.linuxvm.admin_ssh_key
