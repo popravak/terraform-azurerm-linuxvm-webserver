fd7b06c9477a7685aa01664f10cfd7f5b5f2880c
sasa <sasa.popravak@gmail.com>
2022-11-04 20:52:32 +0100

    Interface configuration  included in outputs

--- outputs.tf
+++ outputs.tf
@@ -9,13 +9,17 @@ output "vm_iface_id" {
   value = azurerm_linux_virtual_machine.linuxvm.*.network_interface_ids[0]
 }
 
-output "vm_iface_config_name" {
+output "vm_iface_config" {
   description = "VM interface configuration"
   value = azurerm_network_interface.iface.*.ip_configuration[0].name
 }
 
 /*
-output "ssh_public_key" {
-  value = azurerm_linux_virtual_machine.linuxvm.admin_ssh_key
-}
+on .terraform\modules\linuxvm-webserver\outputs.tf line 14, in output "vm_iface_config_name":
+│   14:   value = azurerm_network_interface.iface.*.ip_configuration[0].name
+│     ├────────────────
+│     │ azurerm_network_interface.iface is tuple with 2 elements
+│
+│ Can't access attributes on a list of objects. Did you mean to access attribute "name" for a specific element of the list, or across all elements of the   
+│ list?
 */
